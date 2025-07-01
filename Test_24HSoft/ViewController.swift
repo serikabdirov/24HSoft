c09ac4ff9be608a0008ce893bce1f86fa1530643
Serik Abdirov <smabdirov99@icloud.com>
2025-07-01 12:55:29 +0300

    Initial Commit

---
+++ Test_24HSoft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_24HSoft
+//
+//  Created by Серик Абдиров on 01.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
